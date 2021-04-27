b5f27a2fcf376edee18ad6a05bc6f85f24a61907
otaviov <amandavv32@gmail.com>
2021-04-27 01:29:46 -0300

    exemplo 1 e 2

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Este é um site em php!</title>
+    </head>
+
+    <body>
+        <?php
+            for ( $i = 0; $i <= 10; $i ++){
+                print("Linha número" .$i. "<br/>");
+            }
+        ?>
+
+    </body>
+
+</html>
